049d314d91bda031a137c8b0515dc035146ecd8a
David Hanks <dphanks@gmail.com>
2020-06-06 22:25:46 -0600

    removed menu from gps-tracker because it blocked requested results.

--- gps-tracker/index.php
+++ gps-tracker/index.php
@@ -1,7 +1,3 @@
-<a href="gps-tracker/?request=updateFullPath">Update Path</a><br />
-<a href="gps-tracker/?request=listall">List all GPS points</a><br />
-<a href="gps-tracker/?request=getPathJSON">get JSON path</a><br />
-<a href="gps-tracker/?request=latest">Latest GPS</a><br />
 <?php
 include('../common/header.php');
 include('gps-tracker.php');
